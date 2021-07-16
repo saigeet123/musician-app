9fe7517163024851cfd8188cab565830c6370132
saigeet123 <79107652+saigeet123@users.noreply.github.com>
2021-07-16 12:41:44 -0400

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const musicianRoutes = require('./routes/musician');
 const app = express();
 const port = process.env.PORT || 3001;
 
-// include routes
+
 app.use('/musician', musicianRoutes);
 
 app.use(express.static('public'));
