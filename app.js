fb9ca045916e4ffab8045e1714b068460a90ee2e
saigeet123 <79107652+saigeet123@users.noreply.github.com>
2021-07-19 16:22:45 -0400

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(express.static('public'))
 
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/build/index.html'));
+  res.sendFile(path.join(__dirname, 'client/build/index.html'))
 });
 
 // initialize store
